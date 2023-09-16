e6cd782e8f6b3be7ef21297f536393b3c693cfcd
Meriem1551 <meriem200409@gmail.com>
2023-09-16 18:38:25 +0200

    update project

--- src/main/java/org/math/Main.java
+++ src/main/java/org/math/Main.java
@@ -1,7 +1,26 @@
 package org.math;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String input ;
+        do {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        } while(!input.equals("quit"));
+
+       //BREAK AND CONTINUE:
+        while (true){ // we must have a break statement
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            if (input.equals("pass"))
+                continue;//return to the loops beginning;
+            if (input.equals("quit"))
+                break;//leave the loop;
+            System.out.println(input);
+        }
     }
 }
